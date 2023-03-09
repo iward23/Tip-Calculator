0d9681c52cc8db7773944a5cdacb538a85823551
Imani Ward <idward712@gmail.com>
2023-03-09 15:03:03 -0500

    o

--- Tip Calculator/ViewController.swift
+++ Tip Calculator/ViewController.swift
@@ -24,8 +24,67 @@ class ViewController: UIViewController {
         billTotal.layer.borderWidth = 3
         billTotal.layer.cornerRadius = 30
     }
-
-UI    }
+    
+    @IBAction func tipSlider_Change(_ sender: UISlider) {
+        tipSliderValue.text = String(Int(tipSlider.value)) + "%"
+    }
+    
+    
+    @IBAction func controlChange(_ sender: UISegmentedControl) {
+        if(tipOptions.selectedSegmentIndex == 3)
+        {
+            tipSlider.isHidden = false
+            tipSliderValue.isHidden = false
+        }
+        else {
+            tipSlider.isHidden = true
+            tipSliderValue.isHidden = true
+        }
+    }
+    
+    
+    @IBAction func resetButton(_ sender: UIButton) {
+        
+    }
+    
+    
+    
+    
+    
+    @IBAction func calculate(_ sender: UIButton) {
+        let sub = Double(subTotal.text!)
+        var tip = 0.0
+        if(tipOptions.selectedSegmentIndex == 0)
+        {
+            tip = 0.15
+        }
+        
+        if(tipOptions.selectedSegmentIndex == 1)
+        {
+            tip = 0.18
+        }
+        
+        if(tipOptions.selectedSegmentIndex == 2)
+        {
+            tip = 0.20
+        }
+        
+        if(tipOptions.selectedSegmentIndex == 3)
+        {
+            tip = Double(tipSlider.value / 100)
+        }
+        let tipA = ((tip * (sub ?? 0) * 100 )) / 100
+        let total = round(((tip * (sub ?? 0 ) * tip) + (sub ?? 0)* 100) / 100
+        grandTotal.text = "$" + String(total)
+        tipAmount.text = "$" + String(tipA)
+    }
+    
+    
+    
+    
+    
+    
+    
     
 }
 
