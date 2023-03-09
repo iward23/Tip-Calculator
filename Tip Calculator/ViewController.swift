37511566ae11d49a9ed940059999f319bb6dcc62
Imani Ward <idward712@gmail.com>
2023-03-09 08:30:50 -0500

    o

--- Tip Calculator/ViewController.swift
+++ Tip Calculator/ViewController.swift
@@ -9,11 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var tipSlider: UISlider!
+    @IBOutlet weak var tipAmount: UILabel!
+    @IBOutlet weak var grandTotal: UILabel!
+    @IBOutlet weak var tipOptions: UISegmentedControl!
+    @IBOutlet weak var subTotal: UITextField!
+    @IBOutlet weak var tipSliderValue: UILabel!
+    @IBOutlet weak var billTotal: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        billTotal.layer.borderWidth = 3
+        billTotal.layer.cornerRadius = 30
     }
 
-
+UI    }
+    
 }
 
